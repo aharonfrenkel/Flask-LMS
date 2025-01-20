043cac46578a5ca025c9da1d4b18e18b14067553
Aharon Frenkel <frenkelahron@gmail.com>
2025-01-20 22:41:11 +0200

    chore: Update models initialization - Add new models to __init__.py - Organize imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,4 +5,7 @@ from .student import Student
 from .teacher import Teacher
 from .course import Course
 from .lecture import Lecture
+from .exercise import Exercise
+from .student_solution import StudentSolution
+from .grade import Grade
 from .associations import StudentCourses, TeacherCourses
